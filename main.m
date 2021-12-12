874dfb65df01199f92475ef3c8b2eff2b37f60fc
WilliamXXu <wl_xxz@protonmail.com>
2021-12-12 01:01:02 +0000

    6

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 %----------------------  adjustable parameters  ----------------------
-iterations=5;
-unit=0.5; % miu m, space step
-timestep=1; %second, tine step
+iterations=1000;
+unit=1; % miu m, space step
+timestep=1e-3; %second, time step
 
 
 %----------------------   parameters ----------------------
@@ -9,10 +9,15 @@ timestep=1; %second, tine step
 %diameter of hepatocyte rescaled, 25-30 miu m
 %diameter of sinusoid rescaled, 10-15 miu m
 %length of sinusoid rescaled, 275 miu m
+
 hepatocyte_diamter=27.5;   
 sinusoid_diameter=12.5;
 sinusoid_length=275;
-
+%{
+hepatocyte_diamter=10;   
+sinusoid_diameter=10;
+sinusoid_length=20;
+%}
 
 sinusoid=round(sinusoid_diameter/2/unit);
 global radius
@@ -23,57 +28,66 @@ global total
 total=(length+2)*(radius+2)    %enlarge the entire matrix by 1 unit, for systematic treatment of boundary conditions
 
 
-%boundary conditions: oxygen concentration and flux at entry (TBD)
-oxy_pp=65;   
-flux=10;  
+%advection and diffusion   
+velo=400;   %miu/sec?
+f=timestep/unit;   %check courant for upwind method'
+if abs(f*velo)>1
+    'courant number>1. upwind method diverges. quit'
+    return
+end
 
-%advection and diffusion
-flow_rate=0.9;   %8-10 mm/sec?
-d1=5;     % diffusion coeff of sinusoid
-d2=0.4;    %diffusion coeff of hepatocyte
+d1=1620;     % diffusion coeff of sinusoid    miu m^2/sec
+d2=1400;    %diffusion coeff of hepatocyte     3600(not sure)
 
-velo=flow_rate*1000/unit;   
-miu1=timestep*d1;
-miu2=timestep*d2;
+miu1=timestep*d1/(unit*unit);
+miu2=timestep*d2/(unit*unit);
 
+%boundary conditions: oxygen concentration and flux at entry
+oxy_pp=65;     %mmHg 
+conversion=1.34e-3;    %to mol/m3, Henry's law
+flux=oxy_pp*600;     %mmHg*miu/sec, slightly higher than velo*oxy_pp
 
  %  Michalis-Menton equation, metabolism for oxygen
 global v_max
-v_max=10;  
+v_max=0.044/conversion;  %mmHg/sec  
 global k_max
-k_max=5;
+k_max=6.24e-3/conversion; %mmHg
 
 
 
 %----------------------  construction of matrices   ----------------------
 % A c^(n+1)= B c^n+ b
-
-c=zeros(length+2,radius+2);
-c(2,2:sinusoid)=oxy_pp;       %Dirichlet boundary condition for sinusoid
-C=vector_assemble(c);     %vector of each grid
-
+C=zeros(total,1);     %vector of each grid
 b=zeros(total,1);
 
 sA=[]; %sparse matrix for iteration matrices A and B
 sB=[]; 
 
 %construction of A and B
-corners=[1 up(1,sinusoid+1) up(1,radius+2) up(length+2,1) up(length+2,sinusoid+1) up(length+2,radius+2)];  %4 corners and 2 ends of the interface set to 0
+corners=[1 up(1,sinusoid+2) up(1,radius+2) up(length+2,1) up(length+2,sinusoid+2) up(length+2,radius+2)];  %4 corners and 2 ends of the interface set to 0
 for k=1:6
     n=corners(k);
     sA=[sA;n n 1];
 end
 
-for j=2:sinusoid   %sinusoid lower/upper boundary, Neumann boundary conditions
-     n=up(1,j);    %lower: flux
-     m=up(3,j);
-     l=up(length+2,j);  % upper: to be fixed
-     sA=[sA;n n 1;n m -1;l l 1];
-     b(n)=-2*flux;
+for j=2:(sinusoid+1)   %sinusoid lower/upper boundary
+     l=up(1,j); 
+     m=up(2,j);
+     n=up(3,j);
 
+     p=up(length+2,j);  
+     q=up(length+1,j);
+     r=up(length,j);
+
+     
+
+     sA=[sA;l m 1;m m f*velo;m n -f*0.5*d1/unit;m l f*0.5*d1/unit;p p 1;q q 1;];
+     sB=[sB;q q 1-f*velo;q r f*velo ];
+     b(l)=oxy_pp;
+     b(m)=f*flux;
 end
 
-for j=(sinusoid+2):(radius+1)   %hepatocyte lower/upper boundary, zero flux
+for j=(sinusoid+3):(radius+1)   %hepatocyte lower/upper boundary, zero flux
      n=up(1,j);   %lower
      m=up(3,j);          
      p=up(length+2,j);  %higher
@@ -85,34 +99,34 @@ end
 
 for i=2:(length+1)    %inside
     n=up(i,1);  %sinusoid left boundary, symmetry
-    sA=[sA;n n 1;n (n+2) -1];
+    sA=[sA;n n 1;n up(i,3) -1];
 
-    for j=2:sinusoid  %sinusoid
+    for j=2:(sinusoid+1)  %sinusoid
+       if and(i>2,i<length+1)
         n=up(i,j);
         sA=[sA;n up(i+1,j) -0.5*miu1;n up(i-1,j) -0.5*miu1;n up(i,j+1) -0.5*miu1;n up(i,j-1) -0.5*miu1;n n 1+2*miu1];
-        sB=[sB;n up(i+1,j) 0.5*miu1;n up(i-1,j) 0.5*miu1+timestep*velo;n up(i,j+1) 0.5*miu1;n up(i,j-1) 0.5*miu1;n n 1-2*miu1-timestep*velo];
-
+        sB=[sB;n up(i+1,j) 0.5*miu1;n up(i-1,j) 0.5*miu1+f*velo;n up(i,j+1) 0.5*miu1;n up(i,j-1) 0.5*miu1;n n 1-2*miu1-f*velo];
+       end
     end
 
-    j=sinusoid+1;    %interface
+    j=sinusoid+2;    %interface
     n=up(i,j);
-    sA=[sA; n n -d1-d2;n up(i,j+1) d2;n up(i,j-1) d1];
+    sA=[sA; n n -miu1-miu2;n up(i,j+1) miu2;n up(i,j-1) miu1];
 
     
-    for j=(sinusoid+2):(radius+1)  %hepatocytes
+    for j=(sinusoid+3):(radius+1)  %hepatocytes
         n=up(i,j);
         sA=[sA;n up(i+1,j) -0.5*miu2;n up(i-1,j) -0.5*miu2;n up(i,j+1) -0.5*miu2;n up(i,j-1) -0.5*miu2;n n 1+2*miu2];
         sB=[sB;n up(i+1,j) 0.5*miu2;n up(i-1,j) 0.5*miu2;n up(i,j+1) 0.5*miu2;n up(i,j-1) 0.5*miu2;n n 1-2*miu2];
     end 
     n=up(i,radius+2);       %hepatocytes right boundary, zero flux
-    sA=[sA;n n 1;n (n-2) -1];
+    sA=[sA;n n 1;n up(i,radius) -1];
 end
 
 %sparse matrix defined
 A=sparse(sA(:,1),sA(:,2),sA(:,3),total,total);
 B=sparse(sB(:,1),sB(:,2),sB(:,3),total,total);
 
-%save('matrices.mat')
 
 %pre-processing to get the matrices for iterations
 A_inv=inv(A);
@@ -143,8 +157,9 @@ end
 
 %map back to 2-d
 res=matrix_assemble(C);
+save('result.mat','res')
 %plot
-heatmap(res);
+%heatmap(res);
 
 
 
@@ -163,25 +178,17 @@ function [i,j]=down(k) %vector index to matrix index
     if j==0
         j=r;
     end
-    i=(k-j)/r;
+    i=(k-j)/r+1;
 
 end
 
 %vector-matrix conversion
 
-function [C]=vector_assemble(c)
-    global total;
-    C=zeros(total,1);
-    for n=1:total
-        C(n)=c(n);
-    end
-end
-
 function [c]=matrix_assemble(C)
     global length;
     global radius;
     global total;
-    c=zeros(length+2,radius+2);
+    c=zeros(radius+2,length+2);
     for n=1:total
         c(n)=C(n);
     end
